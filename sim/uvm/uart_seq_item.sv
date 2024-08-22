b938fadbb654b648a9ae253875ab14777dc0ac6e
shaunsteele <shaun.steele.1020@gmail.com>
2024-08-21 22:05:56 -0700

    added compare function

--- sim/uvm/uart_seq_item.sv
+++ sim/uvm/uart_seq_item.sv
@@ -90,6 +90,45 @@ function string convert2string();
   return s;
 endfunction
 
+function bit compare(uart_seq_item rhs);
+  bit status = 1;
+
+  if (this.write != rhs.write) begin
+    status = 0;
+    `uvm_info(get_full_name(), $sformatf("lhs.write = %b\trhs.write = %b", this.write, rhs.write), UVM_MEDIUM)
+  end else if (this.awdelay != rhs.awdelay || this.awtype != rhs.awtype || this.awaddr != rhs.awaddr || this.awprot != rhs.awprot) begin
+    status = 0;
+    `uvm_info(get_full_name(), $sformatf("lhs.awdelay = %d\trhs.awdelay = %d", this.awdelay, rhs.awdelay), UVM_MEDIUM)
+    `uvm_info(get_full_name(), $sformatf("lhs.awtype = %s\trhs.awtype = %s", this.awtype.name(), rhs.awtype.name()), UVM_MEDIUM)
+    `uvm_info(get_full_name(), $sformatf("lhs.awaddr = 0x%08h\trhs.awaddr = %08h", this.awaddr, rhs.awaddr), UVM_MEDIUM)
+    `uvm_info(get_full_name(), $sformatf("lhs.awprot = 0b%03b\trhs.awprot = 0b%03b", this.awprot, rhs.awprot), UVM_MEDIUM)
+  end else if (this.wdelay != rhs.wdelay || this.wdata != rhs.wdata || this.wstrb != rhs.wstrb) begin
+    status = 0;
+    `uvm_info(get_full_name(), $sformatf("lhs.wdelay = %d\trhs.wdelay = %d", this.wdelay, rhs.wdelay), UVM_MEDIUM)
+    `uvm_info(get_full_name(), $sformatf("lhs.wdata = 0x%08h\trhs.wdata = 0x%08h", this.wdata, rhs.wdata), UVM_MEDIUM)
+    `uvm_info(get_full_name(), $sformatf("lhs.wstrb = 0b%04b\trhs.wstrb = 0b%04b", this.wdata, rhs.wdata), UVM_MEDIUM)
+  end else if (this.bdelay != rhs.bdelay || this.bresp != rhs.bresp) begin
+    status = 0;
+    `uvm_info(get_full_name(), $sformatf("lhs.bdelay = %d\trhs.bdelay = %d", this.bdelay, rhs.bdelay), UVM_MEDIUM)
+    `uvm_info(get_full_name(), $sformatf("lhs.bresp = 0b%02b\trhs.bresp = 0b%02b", this.bresp, rhs.bresp), UVM_MEDIUM)
+  end else if (this.read != rhs.read) begin
+    status = 0;
+    `uvm_info(get_full_name(), $sformatf("lhs.read = %b\trhs.read = %b", this.read, rhs.read), UVM_MEDIUM)
+  end else if (this.ardelay != rhs.ardelay || this.artype != rhs.artype || this.araddr != rhs.araddr || this.arprot != rhs.arprot) begin
+    status = 0;
+    `uvm_info(get_full_name(), $sformatf("lhs.ardelay = %d\trhs.ardelay = %d", this.ardelay, rhs.ardelay), UVM_MEDIUM)
+    `uvm_info(get_full_name(), $sformatf("lhs.artype = %s\trhs.artype = %s", this.artype.name(), rhs.artype.name()), UVM_MEDIUM)
+    `uvm_info(get_full_name(), $sformatf("lhs.araddr = 0x%08h\trhs.araddr = %08h", this.araddr, rhs.araddr), UVM_MEDIUM)
+    `uvm_info(get_full_name(), $sformatf("lhs.arprot = 0b%03b\trhs.arprot = 0b%03b", this.arprot, rhs.arprot), UVM_MEDIUM)
+  end else if (this.rdelay != rhs.rdelay || this.rdata != rhs.rdata || this.rresp != rhs.rresp) begin
+    status = 0;
+    `uvm_info(get_full_name(), $sformatf("lhs.rdelay = %d\trhs.rdelay = %d", this.rdelay, rhs.rdelay), UVM_MEDIUM)
+    `uvm_info(get_full_name(), $sformatf("lhs.rdata = 0x%08h\trhs.rdata = 0x%08h", this.rdata, rhs.rdata), UVM_MEDIUM)
+    `uvm_info(get_full_name(), $sformatf("lhs.rresp = 0b%02b\trhs.rresp = 0b%02b", this.rresp, rhs.rresp), UVM_MEDIUM)
+  end
+  return status;
+endfunction
+
 endclass
 
 `endif
