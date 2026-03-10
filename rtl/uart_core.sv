96cc3ff9d4d6767b38c0a3545206de696336ef85
shaunsteele <shaun.steele.1020@gmail.com>
2026-03-09 23:56:41 -0700

    added fifo buffers to core and finalized pre testbench rtl

--- rtl/uart_core.sv
+++ rtl/uart_core.sv
@@ -3,59 +3,103 @@
 `default_nettype none
 
 module uart_core # (
-    parameter int   DATA_WIDTH = 8
+    parameter int   DATA_WIDTH = 8,
+    parameter int   DVSR_WIDTH = 11,
+    parameter int   SAMPLE_RATE = 16,
+    parameter int   FIFO_ADDR_WIDTH = 2
 )(
-    input   var logic           clk,
-    input   var logic           rst_n,
+    input   var logic                       clk,
+    input   var logic                       rst_n,
 
     // uart phy
-    input   var logic           i_rx,
-    output  var logic           o_tx,
+    input   var logic                       i_rx,
+    output  var logic                       o_tx,
+
+    // baud rate divisor
+    input   var logic   [DVSR_WIDTH-1:0]    i_divisor,
 
     // transmitter control
-    input   var logic                       i_wen,
+    input   var logic                       i_we,
     input   var logic   [DATA_WIDTH-1:0]    i_wdata,
     output  var logic                       o_wfull,
 
     // receiver control
-    input   var logic                       i_ren,
+    input   var logic                       i_re,
     output  var logic   [DATA_WIDTH-1:0]    o_rdata,
     output  var logic                       o_rempty
 );
 
+// baud rate generator
 logic bg_baud_tick;
 baud_gen # (
-    .DVSR_WIDTH ()
+    .DVSR_WIDTH (DVSR_WIDTH)
 ) u_BG (
     .clk        (clk),
     .rst_n      (rst_n),
-    .i_divisor  (),
+    .i_divisor  (i_divisor),
     .o_tick     (bg_baud_tick)
 );
 
-logic rx_done;
+
+// receiver modules
+logic                   rx_done;
+logic [DATA_WIDTH-1:0]  uart_rdata;
+
+fifo #(
+    .DATA_WIDTH (8),
+    .ADDR_WIDTH (FIFO_ADDR_WIDTH)
+) u_RXBUF (
+    .clk        (clk),
+    .rst_n      (rst_n),
+    .i_re       (re),
+    .o_rdata    (o_rdata),
+    .o_rempty   (o_rempty),
+    .i_we       (rx_done),
+    .i_wdata    (rx_rdata),
+    .o_wfull    ()
+);
+
 uart_rx # (
-    .DATA_WIDTH     (),
-    .SAMPLE_RATE    ()
+    .DATA_WIDTH     (DATA_WIDTH),
+    .SAMPLE_RATE    (SAMPLE_RATE)
 ) u_RX (
     .clk            (clk),
     .rst_n          (rst_n),
     .i_rx           (i_rx),
     .i_baud_tick    (bg_baud_tick),
     .o_done         (rx_done),
-    .o_data         (o_rdata)
+    .o_data         (uart_rdata)
 );
 
-logic tx_done;
+
+// transmitter modules
+logic                   tx_done;
+logic [DATA_WIDTH-1:0]  tx_wdata;
+logic                   txbuf_empty;
+
+fifo #(
+    .DATA_WIDTH (8),
+    .ADDR_WIDTH (FIFO_ADDR_WIDTH)
+) u_TXBUF (
+    .clk        (clk),
+    .rst_n      (rst_n),
+    .i_re       (tx_done),
+    .o_rdata    (tx_wdata),
+    .o_rempty   (txbuf_empty),
+    .i_we       (i_we),
+    .i_wdata    (i_wdata),
+    .o_wfull    (o_wfull)
+);
+
 uart_tx # (
-    .DATA_WIDTH     (),
-    .SAMPLE_RATE    ()
+    .DATA_WIDTH     (DATA_WIDTH),
+    .SAMPLE_RATE    (SAMPLE_RATE)
 ) u_TX (
     .clk            (clk),
     .rst_n          (rst_n),
     .i_baud_tick    (bg_baud_tick),
-    .i_start        (),
-    .i_data         (i_wdata),
+    .i_start        (~txbuf_empty),
+    .i_data         (tx_wdata),
     .o_done         (tx_done),
     .o_tx           (o_tx)
 );
